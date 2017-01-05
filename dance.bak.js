0a76842f177babb69260ecb1100ea1410c5a04d1
Bezzle Bug <beezle@pendor.de>
2017-01-05 20:16:30 +0100

    working on new dance.js arch

--- dance.bak.js
+++ dance.bak.js
@@ -37,7 +37,7 @@ footprint.prototype.step = function(x, y, rot, type) {
     this.img.style.top = -y - FOOTPRINT_HEIGHT / 2;
     this.img.style.transform = "rotate(" + rot + "deg)";
     //type
-    this.img.src = footimgs[type];
+    this.img.src = stepimgs[type];
 }
 
 footprint.prototype.fade = function() {
@@ -264,13 +264,17 @@ sequence.prototype.addpoint = function(beat, x, y, rot, sx, sy){
 function sequence_iterator(seq)
 {
     this.seq = seq;
-    this.pos = seq.steps.length == 0 ? null : 0;
-    this.sub = seq.subs.length == 0 ? null : 0;
+    this.pos = seq.steps.length;
+    if(this.pos >= this.steps.length)
+        this.pos = null;
+    this.sub = seq.subs.length;
+    if(this.sub >= this.seq.subs.length)
+        this.pos = null;
     this.sub_iter = null;
     this.sub_off = null;
 }
 
-sequence_iterator.prototype.next = function()
+sequence_iterator.prototype.next()
 {
     if(this.sub_iter != null){
         var itm = this.sub_iter.next();
@@ -285,23 +289,21 @@ sequence_iterator.prototype.next = function()
     var pbeat = this.pos != null ? this.seq.steps[this.pos].beat : null;
     var sbeat = this.sub != null ? this.seq.subs[this.sub].beat : null;
     if(pbeat != null && (sbeat == null || pbeat < sbeat)){
-        var ret =  {step : this.seq.steps[this.pos],
-                    toff : 0,
-                    poff : [0, 0, 0],
-                    mirror : false};
+        ret =  {step : this.seq.steps[this.pos],
+                toff : this.seq.beat,
+                poff : this.seq.pos,
+                mirror : false};
         this.pos += 1;
-        if(this.pos >= this.seq.steps.length)
+        if(this.pos >= this.steps.length)
             this.pos = null;
-        return ret;
     }
     if(sbeat != null && (pbeat == null || sbeat < pbeat)){
         var sub = this.seq.subs[this.sub];
-        this.sub_iter = new sequence_iterator(sub.seq);
+        this.sub_iter = sequence_iterator(this.seq.subs[this.sub]);
         this.sub_off = {pos : sub.pos, beat : sub.beat, mirror : sub.mirror}
         this.sub += 1;
         if(this.sub >= this.seq.subs.length)
-            this.sub = null;
-        return this.next();
+            this.pos = null;
     }
     return null;
 }
@@ -320,13 +322,13 @@ sequence.prototype.getsteps = function(t1, t2, side){
             continue;
         if(t1 <= this.steps[i].beat && t2 > this.steps[i].beat){
             pos = curvepos(self.curve, this.steps[i].beat);
-            var o = {step : this.steps[i], troot : 0, proot : pos, mirror : false};  
+            var o = {step : this.steps[i], troot : 0, proot = pos, mirror : false};  
             ret.push(o);
         }
         if(t1 > this.steps[i].beat)
-            lo = {step : this.steps[i], troot : 0, proot : [0,0,0], mirror : false};
+            lo = {step : this.steps[i], troot : 0, proot = [0,0,0], mirror : false};
         if(hi == 0 && t2 < this.steps[i].beat)
-            hi = {step : this.steps[i], troot : 0, proot : [0,0,0], mirror : false};
+            hi = {step : this.steps[i], troot : 0, proot = [0,0,0], mirror : false};
     }
     for(var i = 0; i < this.subs.length; i++){
         var toff = this.subs[i].beat;
@@ -357,6 +359,95 @@ sequence.prototype.getsteps = function(t1, t2, side){
     return ret;
 }
 
+/*sequence.prototype.getpos = function(now, mir){
+    var pos;
+    //curve
+    var npoints = this.curve.length;
+    if(npoints == 1) //only one point - take this one
+        pos = this.curve[0].pos;
+    else if(npoints >= 2){
+        var n;
+        for(n = 0; n < npoints; n++){
+            if(now < this.curve[n].beat)
+                break;
+        }
+        var tme = now;
+        if(n == 0){ //smaller than first point - take this one
+            tme = this.curve[0].beat;
+            n = 1; 
+        }else if(n >= npoints){ //bigger than last point - take this one
+            tme = this.curve[npoints-1].beat;
+            n = npoints - 1;
+        }
+        pos = bezier(now, this.curve[n-1], this.curve[n]);
+    }else
+        pos = [0, 0, 0];
+    if(mir)
+        pos[0] = -pos[0]; 
+    //recurse on subs
+    var nsubs = this.subs.length;
+    for(n = 0; n < nsubs; n++){
+        var run = this.subs[n];
+        var spos = run.seq.getpos(now - run.beat, mir ^ run.mirror);
+        spos = addpt(run.pos, spos);
+        pos = addpt(pos, spos);
+    }
+    return pos;
+}
+
+sequence.prototype.getsteps = function(t1, t2, troot = 0, mirror = false){
+    var ret = new Array();
+    for(var i = 0; i < this.steps.length; i++){
+        if(t1 <= this.steps[i].beat && t2 > this.steps[i].beat){
+            var o = {step : this.steps[i], beat : troot, mirror : mirror};  
+            ret.push(o);
+        }
+    }
+    for(var i = 0; i < this.subs.length; i++){
+        var off = this.subs[i].beat;
+        var pts = this.subs[i].seq.getsteps(t1 - off, t2 - off, troot + off, false);
+        ret.push.apply(ret, pts); 
+    }
+    return ret;
+}
+
+sequence.prototype.getframe = function(now, troot = 0, croot = null, mirror = false){
+    var last = null;
+    var curvpos = this.curve.get(now - troot);
+    for(var i = 0; i < this.steps.length; i++){
+        if(now < this.steps[i].beat){
+            return {'lo' : last, 'hi' : this.steps[i]}
+        }
+        last = this.steps[i];
+    }
+    for(var i = 0; i < this.subs.length; i++){
+        var off = this.subs[i].beat;
+        var rsub = this.subs[i].seq.getframe(now - off, troot + off, false);
+        if(rsub != null){
+            if(rsub['lo'] == null)
+                rsub['lo'] = last;
+            if(rsub['hi'] == null)
+                last = rsub['lo'];
+            else
+                return rsub
+        }
+    }
+    return {'lo' : last, 'hi' : null};
+}
+
+sequence.prototype.getshove = function(now){
+    fram = this.getframe(now);
+    pos = this.getpos(now);
+    if(fram['lo'] == null)
+        step = fram['hi'];
+    else if(fram['hi'] == null)
+        step = fram['lo']
+    else{
+
+    }
+    
+}*/
+
 //////////////////////////////////
 
 // DANCER
@@ -368,30 +459,36 @@ function dancer(x, y, r, clk) {
     this.right = new foot();
     this.pointer = new pointer();
     this.seq = new sequence();
-    this.it = null;
-    this.step = null;
     //init
     this.clk.register(this, null);
 }
 
 dancer.prototype.clk_call = function (now) {
-    if(this.it == null)
-        this.it = new sequence_iterator(this.seq);
-    if(this.step == null){
-        this.step = this.it.next();
-        if(this.step == null)
-            this.it = null;
-    }
-    if(this.step != null && this.step.step.beat + this.step.toff < now){
-        this.dostep();
-        this.step = null;
-    }
+    var pos = addpt(this.pos, this.seq.getpos(now));
+    this.pointer.move(pos[0], pos[1], pos[2]);
+    //right
+    var pts = this.seq.getsteps(this.lasttick, now, 'r');
+    var stps = pts.slice(1, pts.length-1);
+    for(var pt = 0; pt < stps.length; pt++)
+        this.dostep(stps[pt].step, stps[pt].beat, stps[pt].mirror);
+    var shdwpos = step_interpolate(pts[pts.length-1], pts[pts.length-2], now);
+    this.right.move(shdowpos[0], shdowpos[1], shdowpos[2]);    
+    //left
+    pts = this.seq.getsteps(this.lasttick, now, 'l');
+    stps = pts.slice(1, pts.length-1);
+    for(var pt = 0; pt < stps.length; pt++)
+        this.dostep(stps[pt].step, stps[pt].beat, stps[pt].mirror);
+    shdwpos = step_interpolate(pts[pts.length-1], pts[pts.length-2], now);
+    this.left.move(shdowpos[0], shdowpos[1], shdowpos[2]);    
+    //
+    this.lasttick = now;
+    return null;
 }
 
-dancer.prototype.dostep = function() {
+dancer.prototype.dostep = function(step, beat, mirror) {
     var ft, ftx, ftt;
-    ftt = this.step.step.foot;
-    if(this.step.mirror)
+    ftt = step.foot;
+    if(mirror)
         ftt = ftt == 'l' ? 'r' : 'l';
     if(ftt == 'l'){
         ft = this.left;
@@ -400,10 +497,102 @@ dancer.prototype.dostep = function() {
         ft = this.right;
         ftx = DANCER_RIGTH_XOFF;
     }
-    var pos = addpt(this.step.step.pos, this.step.poff);
+    var pos = addpt(this.pos, this.seq.getpos(beat + step.beat));
+    pos = addpt(pos, step.pos, mirror); //add step pos
     pos = addpt(pos, [ftx, 0, 0]);
-    ft.step(pos[0], pos[1], pos[2], this.step.step.typ);
+    ft.step(pos[0], pos[1], pos[2], step.typ);
 }
+
+//item: step troot proot mirror
+//step: beat, pos, foot, typ
+function step_interpolate(st1, st2, now)
+{
+    if(st1 == null)
+        return addpt(st2.step.pos, st2.proot);
+    if(st2 == null)
+        return addpt(st1.step.pos, st1.proot);
+    var r = (now - (st1.step.beat + st1.troot)) / ((st2.step.beat + st2.troot) - (st1.step.beat + st2.troot));
+    var p1 = addpt(st1.step.pos, st1.proot);
+    var p2 = addpt(st2.step.pos, st2.proot);
+    var ret = [
+        p1[0] + (p2[0] - p1[0]) * r,
+        p1[1] + (p2[1] - p1[1]) * r,
+        p1[2] + (p2[2] - p1[2]) * r,
+    ];
+    return ret;
+}
+
+/*dancer.prototype.getpos = function(now)
+{
+    var s, p;
+    var cmov = null;
+    var t2, p2;
+    var p1 = null, t1 = null;
+    for(s = 0; s < this.seq.length; s++){
+        var sitm = this.seq[s];
+        var intvl = sitm.seq.curve.interval();
+        if(now - sitm.beat > intvl[1] && s + 1 < this.seq.length)
+            continue;
+        t2 = intvl[0] + sitm.beat;
+        p2 = sitm.seq.curve.get(now - sitm.beat);
+        if(sitm.mirror)
+            p2[0] = -p2[0];
+        if(now < t2 && s != 0){
+            t1 = this.seq[s-1].seq.curve.interval()[1] + this.seq[s-1].beat;
+            p1 = this.seq[s-1].seq.curve.get(now - this.seq[s-1].beat);
+            if(t1 == t2){
+                cmov = p2;
+                break;
+            }
+            var rat = (now - t1) / (t2 - t1);
+            var p1 = this.seq[s-1].seq.curve.points[this.seq[s-1].seq.curve.points.length-1];
+            var x = (p2.x - p1.x) * rat;
+            var y = (p2.y - p1.y) * rat;
+            var r = (p2.r - p1.r) * rat;
+            cmov = [x, y, r];
+            break;
+        }
+        cmov = p2;
+        break;
+    }
+    var pos = addpt(this.pos, this.curve.get(now));
+    pos = addpt(pos, cmov);
+    return pos;
+}
+    
+
+dancer.prototype.addseq = function(x, y, r, begin, seq, mirror) {
+    seqitem = {
+        seq : seq,
+        pos : [x, y, r],
+        beat : begin,
+        mirror : mirror
+    };
+    this.seq.push(seqitem);
+}
+
+dancer.prototype.dostep = function(sq, stp) {
+    var st = this.seq[sq].seq.steps[stp];
+    var mr = this.seq[sq].mirror;
+    var ft, ftx, ftt;
+    ftt = st.foot;
+    if(mr)
+        ftt = ftt == 'l' ? 'r' : 'l';
+    if(ftt == 'l'){
+        ft = this.left;
+        ftx = DANCER_LEFT_XOFF;
+    }else{
+        ft = this.right;
+        ftx = DANCER_RIGTH_XOFF;
+    }
+    var pos = addpt(this.pos, this.curve.get(st.beat + this.seq.beat)) //add dancer curve
+    pos = addpt(pos, this.seq[sq].pos) //add sequence anchor
+    pos = addpt(pos, this.seq[sq].seq.curve.get(st.beat), mr); //add seuence curve
+    pos = addpt(pos, st.pos, mr); //add step pos
+    pos = addpt(pos, [ftx, 0, 0]);
+    ft.step(pos[0], pos[1], pos[2], st.typ);
+}
+*/
 
 //////////////////////////////////
 
@@ -452,7 +641,7 @@ function clock_call(clock) {
             clock.begin += (clock.end - clock.start) * 1000 / clock.speed;
             clock.lasttick -= clock.end - clock.start;
             console.log("clk wrap " + clock.begin);
-            //clock_call(clock);
+            clock_call(clock);
         }else{
             console.log("clk stop");
             clearTimeout(clock.sched_tout);
